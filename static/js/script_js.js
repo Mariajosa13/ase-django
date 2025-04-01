b07742c140994d6ab1de2016d35b9385d67a3794
Mariajosa13 <carojm2006@gmail.com>
2025-04-01 14:15:14 -0500

    se modifica seccion de slider

--- static/js/script_js.js
+++ static/js/script_js.js
@@ -2,8 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const carousel = document.querySelector(".carousel");
     const arrowBtns = document.querySelectorAll(".wrapper i");
     const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+    const carouselChildrens = [...carousel.children];
 
     let isDragging = false, startX, startScrollLeft;
+
+    let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+    carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+        carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+    })
+
+    carouselChildrens.slice(0, cardPerView).forEach(card => {
+        carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+    })
 
     arrowBtns.forEach(btn => {
         btn.addEventListener("click", () => {
@@ -28,10 +39,20 @@ document.addEventListener("DOMContentLoaded", function () {
         carousel.classList.remove("dragging");
     }
 
+    const infiniteScroll = () => {
+        if(carousel.scrollLeft === 0){
+            carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth);
+        } else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+            carousel.scrollLeft = carousel.offsetWidth;
+        }
+    }
+
     
     carousel.addEventListener("mousedown", dragStart);
     carousel.addEventListener("mousemove", dragging);
-    carousel.addEventListener("mouseup", dragStop);
+    document.addEventListener("mouseup", dragStop);
+    carousel.addEventListener("scroll", infiniteScroll);
+
 });
 
 
